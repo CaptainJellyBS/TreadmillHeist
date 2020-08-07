69ac727fe08005be68245f580cbae2e90b64eec8
Mana <manakouwenberg@gmail.com>
2020-08-07 06:27:07 +0200

    Made treadmills and an object that is carried on them

---
+++ TreadmillGame/Assets/Scripts/MovableObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector3 treadVector;
+    Treadmill curMill;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(curMill == null) { treadVector = Vector3.zero; }
+        else { treadVector = curMill.GetTreadVector(); }
+        transform.position += treadVector * Time.deltaTime;
+    }
+
+    void OnCollisionStay(Collision col)
+    {
+        if(curMill != null) { return; }
+        if(col.collider.tag == "Treadmill")
+        {
+            curMill = col.collider.GetComponent<Treadmill>();
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.collider.tag == "Treadmill")
+        {
+            curMill = null;
+        }
+    }
+}
---
+++ TreadmillGame/Assets/Scripts/Treadmill.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Treadmill : MonoBehaviour
+{
+    [Range(-1,1)]
+    public int speed;
+
+
+    Vector3 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = transform.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Vector3 GetTreadVector()
+    {
+        return direction * speed;
+    }
+}
